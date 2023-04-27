3b267893d9e6a1f9c86d177d213aec3e96245ea3
LIN_CHIEH <linchieh@stu.comp.nus.edu.sg>
2023-04-27 22:42:54 +0800

    Implement expandable sidebar, upgrade appbar

--- streamscheduler/test/widget_test.dart
+++ streamscheduler/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:streamscheduler/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const StreamScheduler());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
