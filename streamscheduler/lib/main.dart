de674dc724e58138508d76937da3871b3fc02b1b
LIN_CHIEH <linchieh@stu.comp.nus.edu.sg>
2023-04-28 18:05:47 +0800

    Refactor code

--- streamscheduler/lib/main.dart
+++ streamscheduler/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'src/home_page.dart' as home_page;
-import 'src/main_app_state.dart' as main_app_state;
+import 'src/main_application.dart' as home_page;
+import 'src/shared_app_state.dart' as shared_app_state;
 
 void main() {
   runApp(const StreamScheduler());
@@ -13,14 +13,14 @@ class StreamScheduler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => main_app_state.MainAppState(),
+      create: (context) => shared_app_state.MainAppState(),
       child: MaterialApp(
         title: 'StreamScheduler',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
         ),
-        home: const home_page.HomePage(title: 'StreamScheduler'),
+        home: const home_page.MainApplication(title: 'StreamScheduler'),
       ),
     );
   }
---
+++ streamscheduler/lib/src/main_application.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'pages/subscriptions_page.dart';
+
+class MainApplication extends StatefulWidget {
+  const MainApplication({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MainApplication> createState() => _MainApplicationState();
+}
+
+class _MainApplicationState extends State<MainApplication> {
+  bool _isExpanded = false;
+  var _selectedIndex = 0;
+
+  void _toggleNavigationBar() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.bodyMedium!.copyWith(
+      color: theme.colorScheme.background,
+    );
+
+    Widget page;
+    switch (_selectedIndex) {
+      case 0:
+        page = HomePage();
+        break;
+      case 1:
+        page = SubscriptionsPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $_selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: theme.colorScheme.primaryContainer,
+          title: Text(widget.title),
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: _toggleNavigationBar,
+          ),
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: _isExpanded,
+                  minExtendedWidth: 200,
+                  backgroundColor: theme.colorScheme.onBackground,
+                  destinations: [
+                    NavigationRailDestination(
+                        icon: Icon(
+                          Icons.home_outlined,
+                          color: theme.colorScheme.background,
+                        ),
+                        selectedIcon: Icon(
+                          Icons.home,
+                          color: theme.colorScheme.onBackground,
+                        ),
+                        label: Text(
+                          "Home",
+                          style: style,
+                        )),
+                    NavigationRailDestination(
+                        icon: Icon(
+                          Icons.subscriptions_outlined,
+                          color: theme.colorScheme.background,
+                        ),
+                        selectedIcon: Icon(
+                          Icons.subscriptions,
+                          color: theme.colorScheme.onBackground,
+                        ),
+                        label: Text("Subscriptions", style: style)),
+                  ],
+                  selectedIndex: _selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      _selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(child: page,),
+              ),
+            ],
+          ),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print('pressed button'),
+          tooltip: 'placeholder',
+          child: const Icon(Icons.add),
+        ),
+      );
+    });
+  }
+}
