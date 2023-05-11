c75fe374cf473902f86306117c39973f81722995
euph00 <euph00@users.noreply.github.com>
2023-05-11 07:47:31 +0000

    deploy: e1e73a77ce86db8c2a98ab8b79f355f2c0c9ddda

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,29 +3,29 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "36b7c5871995585a32f8557c86dc5375",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/assets/images/developed-with-youtube-sentence-case-dark.png": "e8505a1c2269d1b115ffb66529c93221",
-"assets/assets/images/btn_google_signin_dark_pressed_web@2x.png": "75a7cb04887751c08cba916885e3b5fd",
-"assets/assets/images/btn_google_signin_dark_normal_web@2x.png": "56f00e35d133cf44f6590c095ce984da",
-"assets/NOTICES": "53078d27737b564da755f8a53bebfb4a",
-"assets/AssetManifest.json": "e3a0894416e4edf4a79e7c2892f27d89",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"main.dart.js": "64a7ef4f932eca867312c3fd78bd303b",
-"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+  "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
-"manifest.json": "8e8bf38a75f208f4aa4879598de3570d",
-"index.html": "f508d2ae76ad44bb2f0cb1aadbe7d536",
-"/": "f508d2ae76ad44bb2f0cb1aadbe7d536",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"assets/assets/images/btn_google_signin_dark_pressed_web@2x.png": "75a7cb04887751c08cba916885e3b5fd",
+"assets/assets/images/btn_google_signin_dark_normal_web@2x.png": "56f00e35d133cf44f6590c095ce984da",
+"assets/assets/images/developed-with-youtube-sentence-case-dark.png": "e8505a1c2269d1b115ffb66529c93221",
+"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
+"assets/NOTICES": "e53cccccfb904fc7a40081eab32f70ff",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/AssetManifest.json": "e3a0894416e4edf4a79e7c2892f27d89",
+"index.html": "0db0c54a2b292f70368327ec04db4f1a",
+"/": "0db0c54a2b292f70368327ec04db4f1a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"version.json": "36b7c5871995585a32f8557c86dc5375",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea"
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"main.dart.js": "4f8ec00584bd16209302afea87188ad2",
+"manifest.json": "8e8bf38a75f208f4aa4879598de3570d"
 };
 
 // The application shell files that are downloaded before a service worker can
